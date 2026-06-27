cc4be82dc2f85956dfd8c8c0722e3c2bc35a8403
Rosalba <rosalbagarciaosorio151998@gmail.com>
2026-06-27 15:43:14 -0300

    tercer guaradado

--- captura_datos.py
+++ captura_datos.py
@@ -1,9 +1,57 @@
 from database_manager import obtener_conexion
 
+def registrar_producto(nombre,descripcion,cantidad,precio,categoria):
 
-
-
-def registrar_producto():
-    
     conexion=obtener_conexion()
     cursor = conexion.cursor()
+
+    cursor.execute(
+        '''
+        INSERT INTO productos(nombre,descripcion,cantidad,precio,categoria)
+        VALUES(?,?,?,?,?)
+    ''',
+         (nombre,descripcion,cantidad,precio,categoria),        
+                   
+     )
+   
+    conexion.commit()
+    conexion.close()
+
+def eliminar_producto(id):
+     conexion=obtener_conexion()
+     cursor = conexion.cursor()
+
+     cursor.execute('DELETE FROM productos where id =?',(id,))
+          
+     conexion.commit()
+     conexion.close()
+
+def listar_productos():
+    conexion=obtener_conexion()
+    cursor = conexion.cursor()
+    cursor.execute("SELECT * FROM productos")
+    resultados=cursor.fetchall()
+    conexion.close()
+    return resultados
+
+
+def buscar_productos():
+    conexion=obtener_conexion()
+    cursor = conexion.cursor()
+    cursor.execute("SELECT * FROM productos WHERE id > ?",(id,))
+    resultado=cursor.fetchone()
+    conexion.close()
+    return resultado
+
+
+def editar_producto():
+    pass
+       
+       
+                    
+                    
+                    
+                
+
+     
+    
